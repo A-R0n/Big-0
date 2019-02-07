f20c70a7f56c95b442c8aa0d59b4aeffef1037e2
Aaron Estes <aestesc@gmail.com>
2019-02-06 20:27:09 -0600

    test it out...

--- BigO.py
+++ BigO.py
@@ -1,23 +1,34 @@
-# Python 3.6
+# Python 3.7
 # Big O Notation examples of functions with constant, linear, and quadratic time complexity
 
+import time
+
+
 trees = ['oak', 'sequoia', 'pine', 'beach', 'palm', 'boddhisatva', 'maple']
-treeFacts = [['oak', 7], ['palm', 1], ['palm', 2], ['beach', 5], ['maple', 2]]
+treeFacts = [['oak', 3], ['palm', 1], ['palm', 2], ['beach', 5], ['maple', 7]]
 
-def dumb_function(trees):
+# constant time complexity --> O(1)
+def dumbFunction(x):
     emptyBox = 0
-    return emptyBox
+    print(emptyBox)
 
-def linear_complexity(trees):
-    for i in trees:
-        print(i)
-    
-def quadratic_complexity(treeFacts):
-    for tree in treeFacts:
-        for purchase in tree:
-            if purchase[1] == 1:
-                receipt = purchase[0]
-            if receipt == 'oak':
-                print(receipt)
-        
+# linear time complexity --> O(n)
+def linearComplexity(x):
+    for tree in x:
+        print(tree)
+
+# quadratic time complexity --> 0(N squared)
+def quadraticComplexity(x):
+    start = time.time()
+    for sale in x:
+        for description in sale:
+            if description == 7:
+                end = time.time()
+                print(end-start)
+            else:
+                continue
+
+dumbFunction(trees) # 0
+linearComplexity(trees) # oak, sequoia, pine, beach, palm, boddhisatva, maple
+quadraticComplexity(treeFacts) # how long will it take to find the correct receipt?
 
