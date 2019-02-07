0a35ebb80769a2ef5f0e277eca659b4aba8f0494
Aaron Estes <aestesc@gmail.com>
2019-02-06 19:51:44 -0600

    quadratic time complexity O(n squared)

--- BigO.py
+++ BigO.py
@@ -1,14 +1,25 @@
 
 
-trees = ['oak', 'saquoa', 'pine', 'beach', 'palm', 'coniferous', 'deciduous']
+trees = ['oak', 'sequoia', 'pine', 'beach', 'palm', 'boddhisatva', 'maple']
+treeFacts = [['oak', 7], ['palm', 1], ['palm', 2], ['beach', 5], ['maple', 2]]
 
-def dumb_function(trees) {
+def dumb_function(trees):
     emptyBox = 0
     return emptyBox
-}
 
-def linear_complexity(trees) {
-    for i in trees {
+
+def linear_complexity(trees):
+    for i in trees:
         print(i)
-    }
-}
+    
+
+
+def quadratic_complexity(treeFacts):
+    for tree in treeFacts:
+        for purchase in tree:
+            if purchase[1] == 1:
+                receipt = purchase[0]
+            if receipt == 'oak':
+            print(receipt)
+        
+
