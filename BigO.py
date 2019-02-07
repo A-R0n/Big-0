9257cb66801e3d1a1aae096746612396fcefdeda
Aaron Estes <aestesc@gmail.com>
2019-02-06 19:53:03 -0600

    summary at top...styling

--- BigO.py
+++ BigO.py
@@ -1,4 +1,5 @@
-
+# Python 3.6
+# Big O Notation examples of functions with constant, linear, and quadratic time complexity
 
 trees = ['oak', 'sequoia', 'pine', 'beach', 'palm', 'boddhisatva', 'maple']
 treeFacts = [['oak', 7], ['palm', 1], ['palm', 2], ['beach', 5], ['maple', 2]]
@@ -7,13 +8,10 @@ def dumb_function(trees):
     emptyBox = 0
     return emptyBox
 
-
 def linear_complexity(trees):
     for i in trees:
         print(i)
     
-
-
 def quadratic_complexity(treeFacts):
     for tree in treeFacts:
         for purchase in tree:
